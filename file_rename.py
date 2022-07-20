84bf6c58dc6deb6e662341bc4c6b3d835839921f
leena <leenagyeom91@gmail.com>
2022-07-20 22:02:29 +0900

    label 파일 리스트와 이미지 파일 매칭해서 rename

--- file_rename.py
+++ file_rename.py
@@ -2,15 +2,17 @@ import os
 import cv2
 import numpy as np
 
-img_list = os.listdir("../raw_image/L500")
-lab_list = os.listdir("../raw_image/L500_labels")
+img_path = "./raw_image/P1500"
+lab_path = "./raw_image/P1500_labels"
+img_list = os.listdir(img_path)
+lab_list = os.listdir(lab_path)
 
-for e, i in enumerate(img_list, start=1):
-    ni = i.split('jpg')[0][:-1]
-    for l in lab_list:
-        nl = l.split('txt')[0][:-1]
+for e, l in enumerate(lab_list, start=2486):
+    nl = l.split('txt')[0][:-1]
+    for i in img_list:
+        ni = i.split('jpg')[0][:-1]
         if ni == nl :
             new_i = 'img_'+str(e)+'.png'
             new_l = 'img_'+str(e)+'.txt'
-            os.rename(f"../raw_image/L500/{i}", f"../raw_image/L500_newimg/{new_i}")
-            os.rename(f"../raw_image/L500_labels/{l}", f"../raw_image/L500_newlab/{new_l}")
+            os.rename(f"./{img_path}/{i}", f"./{img_path}/{new_i}")
+            os.rename(f"./{lab_path}/{l}", f"./{lab_path}/{new_l}")
